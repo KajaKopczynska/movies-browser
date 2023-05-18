dabc7947a4de9cffe24741f06a6cb87e011f862c
KajaKopczynska <kaja.a.kopczynska@gmail.com>
2023-05-18 12:33:31 +0200

    theme.js added

---
+++ src/theme.js
@@ -0,0 +1,24 @@
+export const theme = {
+    colors: {
+        white: "#FFFFFF",
+        whisper: "#F5F5FA",
+        mystic: "#E4E6F0",
+        silver: "#C4C4C4",
+        pattensBlue: "#D6E4FF",
+        waterloo: "#7E839A",
+        snuff: "#DDDDEE",
+        linkWater: "#D3DEF3",
+        scienceBlue: "#0044CC",
+        stormGray: "#74788B",
+        smoke: "#18181B",
+        black: "#000000",
+    },
+
+    breakpoints: {
+        medium: 555,
+        large: 992,
+        max: 1250,
+        mobileMin: 471,
+        mobileMax: 767,
+    },
+};
