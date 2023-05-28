8a98f37049b00d58780f1ca6246fdd62d2de9a84
KajaKopczynska <kaja.a.kopczynska@gmail.com>
2023-05-28 10:59:09 +0200

    useQueryParameter hook added

---
+++ src/useQueryParameter.js
@@ -0,0 +1,10 @@
+import { useLocation } from "react-router-dom";
+
+const useQueryParameter = (paramName) => {
+    const location = useLocation();
+    return new URLSearchParams(location.search).get(paramName);
+};
+
+export const searchQueryParamName = "search";
+
+export default useQueryParameter;
