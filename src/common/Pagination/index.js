854cab24be6afcf7a0432998819b1a154c4acd0c
KajaKopczynska <kaja.a.kopczynska@gmail.com>
2023-05-28 11:22:06 +0200

    functionalities to pagination added

--- src/common/Pagination/index.js
+++ src/common/Pagination/index.js
@@ -1,49 +1,49 @@
 import { useSearchParams } from "react-router-dom";
 import { Wrapper, Button, ArrowIconLeft, ButtonText, PageCounter, PageNumbers, ArrowIconRight } from "./styled";
 
+const searchQueryParamName = "search";
+
 export const Pagination = ({ location, totalPages }) => {
 
     const [searchParams, setSearchParams] = useSearchParams();
     const currentPage = Number(searchParams.get("page")) || 1;
+    const query = searchParams.get(searchQueryParamName);
     const totalPage = totalPages > 500 ? 500 : totalPages;
 
     const onGoToFirst = () => {
         if (currentPage !== 1) {
-            //   query
-            //     ? setSearchParams({ [searchQueryParamName]: query, page: 1 })
-            //     : 
-            setSearchParams({ page: 1 });
+            query
+                ? setSearchParams({ [searchQueryParamName]: query, page: 1 })
+                : setSearchParams({ page: 1 });
         }
     };
 
     const onGoToPrevious = () => {
         if (currentPage !== 1) {
             const previousPage = currentPage - 1;
-            //   queryArrowIconLeft
-            //     ? setSearchParams({ [searchQueryParamName]: query, page: previousPage })
-            //     : 
-            setSearchParams({ page: previousPage });
+            query
+                ? setSearchParams({ [searchQueryParamName]: query, page: previousPage })
+                : setSearchParams({ page: previousPage });
         }
     };
 
     const onGoToNext = () => {
         if (currentPage !== totalPages) {
             const nextPage = currentPage + 1;
-            //   query
-            //     ? setSearchParams({ [searchQueryParamName]: query, page: nextPage })
-            //     : 
-            setSearchParams({ page: nextPage });
+            query
+                ? setSearchParams({ [searchQueryParamName]: query, page: nextPage })
+                : setSearchParams({ page: nextPage });
         }
     };
 
     const onGoToLast = () => {
         if (currentPage !== totalPage) {
-            //   query
-            //     ? setSearchParams({ [searchQueryParamName]: query, page: totalPage })
-            //     : 
-            setSearchParams({ page: totalPage });
+            query
+                ? setSearchParams({ [searchQueryParamName]: query, page: totalPage })
+                : setSearchParams({ page: totalPage });
         }
     };
+    
     return (
         <Wrapper location={location}>
             <Button disabled={currentPage <= 1} onClick={onGoToFirst}>
@@ -61,11 +61,11 @@ export const Pagination = ({ location, totalPages }) => {
                 of
                 <PageNumbers>{totalPage}</PageNumbers>
             </PageCounter>
-            <Button disabled={currentPage >= 1} onClick={onGoToNext}>
+            <Button disabled={currentPage >= totalPages} onClick={onGoToNext}>
                 <ButtonText>Next</ButtonText>
                 <ArrowIconRight />
             </Button>
-            <Button disabled={currentPage >= 1} onClick={onGoToLast}>
+            <Button disabled={currentPage >= totalPages} onClick={onGoToLast}>
                 <ButtonText>Last</ButtonText>
                 <ArrowIconRight />
                 <ArrowIconRight mobile="true" />
