7ce95ac706ced1ad7b19308e6b608fc588e8c582
KajaKopczynska <kaja.a.kopczynska@gmail.com>
2023-05-30 12:33:28 +0200

    Added routes for MoviesList and MoviePage components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import { Routes, Route, Navigate } from "react-router-dom";
 import Header from "./common/Header"
 import MoviesList from "./features/movies/MoviesList";
+import { MoviePage } from "./features/movies/MoviePage";
 
 function App() {
   return (
     <>
       <Header />
-      <MoviesList></MoviesList>
-      
-
-
+      <Routes>
+        <Route path="/" element={<Navigate to="/movies" />} />
+        <Route path="/movies" element={<MoviesList />} />
+        <Route path="/movies/:id" element={<MoviePage />} />
+      </Routes>
     </>
   );
 }
