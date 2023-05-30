06931a63025edc6c4e25334122a24bb1484d0f5b
KajaKopczynska <kaja.a.kopczynska@gmail.com>
2023-05-30 14:04:47 +0200

    Added MoviePage functionality and navigation

--- src/features/movies/MoviePage/index.js
+++ src/features/movies/MoviePage/index.js
@@ -1,9 +1,40 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams, useSearchParams } from "react-router-dom";
+import { fetchMovieLoading, selectMovieStatus } from "./movieSlice";
 import { Name } from "./styled";
+import { Loading } from "../../../common/status/Loading";
+import { Error } from "../../../common/status/Error";
+import { searchQueryParamName } from "../../../useQueryParameter";
 
 export const MoviePage = () => {
+    const dispatch = useDispatch();
+    const { id } = useParams();
+    const stateOfLoading = useSelector(selectMovieStatus);
+    const navigate = useNavigate();
+    const [searchParams] = useSearchParams();
+    const query = searchParams.get(searchQueryParamName);
+
+    useEffect(() => {
+        dispatch(fetchMovieLoading(id));
+        if (query) {
+            navigate(`/movies?${searchQueryParamName}=${query}`);
+        }
+    }, [dispatch, id, query, navigate]);
+
     return (
-        <Name>
-            TEST TEST TEST
-        </Name>
+        <>
+            {stateOfLoading === "loading" ? (
+                <Loading />
+            ) : stateOfLoading === "error" ? (
+                <Error />
+            ) : (
+                <>
+                    <Name>
+                        TEST TEST TEST
+                    </Name>
+                </>
+            )}
+        </>
     );
 };
