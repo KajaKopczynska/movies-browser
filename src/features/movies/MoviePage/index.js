43660039fd642fdcd0a7d6143c2f61276efd580c
KajaKopczynska <kaja.a.kopczynska@gmail.com>
2023-05-30 12:31:38 +0200

    Added a test component: MoviePage

---
+++ src/features/movies/MoviePage/index.js
@@ -0,0 +1,9 @@
+import { Name } from "./styled";
+
+export const MoviePage = () => {
+    return (
+        <Name>
+            TEST TEST TEST
+        </Name>
+    );
+};
---
+++ src/features/movies/MoviePage/styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+const mobileMaxBP = ({ theme }) => theme.breakpoints.mobileMax;
+
+export const Name = styled.h2`
+  font-weight: 600;
+  font-size: 36px;
+  line-height: 120%;
+  margin: 0;
+
+  @media (max-width: ${mobileMaxBP}px) {
+    font-weight: 500;
+    font-size: 16px;
+  }
+`;
