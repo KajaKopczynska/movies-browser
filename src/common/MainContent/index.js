4b365f612d5cbdb5253b8712303548510a82d167
KajaKopczynska <kaja.a.kopczynska@gmail.com>
2023-05-30 11:29:44 +0200

    MainContent component added

---
+++ src/common/MainContent/index.js
@@ -0,0 +1,22 @@
+import styled, { css } from "styled-components";
+
+const mobileBP = ({ theme }) => theme.breakpoints.mobileMax;
+
+export const MainContent = styled.main`
+  padding: 56px 16px 40px;
+
+  @media (max-width: ${mobileBP}px) {
+    padding: 24px 16px 32px;
+  }
+  
+  ${({ moviePage }) =>
+    moviePage &&
+    css`
+      padding-top: 0;
+      padding-bottom: 336px;
+
+      @media (max-width: ${mobileBP}px) {
+        padding-bottom: 88px;
+      }
+    `}
+`;
