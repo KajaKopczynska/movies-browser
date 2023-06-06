40e1d31a8d27c3177b9e77a64c8e1515364da599
KajaKopczynska <kaja.a.kopczynska@gmail.com>
2023-06-06 17:49:12 +0200

    personSlice created

---
+++ src/features/people/PersonPage/personSlice.js
@@ -0,0 +1,46 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const personSlice = createSlice({
+    name: "person",
+    initialState: {
+        status: "loading",
+        person: [],
+        cast: [],
+        crew: [],
+    },
+    reducers: {
+        fetchPersonLoading: () => ({
+            status: "loading",
+            person: [],
+            cast: [],
+            crew: [],
+        }),
+        fetchPersonError: () => ({
+            status: "error",
+            person: [],
+            cast: [],
+            crew: [],
+        }),
+        fetchPersonSuccess: (_, { payload: { person, personDetails } }) => ({
+            status: "success",
+            person: person,
+            cast: personDetails.cast,
+            crew: personDetails.crew,
+        }),
+    },
+});
+
+export const {
+    fetchPersonLoading,
+    fetchPersonError,
+    fetchPersonSuccess,
+} = personSlice.actions;
+
+const selectPersonState = (state) => state.person;
+
+export const selectPerson = (state) => selectPersonState(state).person;
+export const selectPersonStatus = (state) => selectPersonState(state).status;
+export const selectPersonCast = (state) => selectPersonState(state).cast;
+export const selectPersonCrew = (state) => selectPersonState(state).crew;
+
+export default personSlice.reducer;
