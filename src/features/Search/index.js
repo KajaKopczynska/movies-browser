004dab309c81c22e0821d01e92c7455ca2d29e34
KajaKopczynska <kaja.a.kopczynska@gmail.com>
2023-06-15 12:49:22 +0200

    added event handlers and dispatch search action in Search

--- src/features/Search/index.js
+++ src/features/Search/index.js
@@ -1,12 +1,41 @@
+import { useDispatch } from "react-redux";
+import { useLocation } from "react-router-dom";
+import { useReplaceQueryParameter } from "../../useReplaceQueryParameter";
 import { SearchWrapper, SearchIcon, SearchInput } from "./styled";
+import useQueryParameter, { searchQueryParamName } from "../../useQueryParameter";
+import { fetchSearchSuccess } from "./searchSlice";
 
 export const Search = () => {
+    const dispatch = useDispatch();
+    const location = useLocation();
+
+    const query = useQueryParameter(searchQueryParamName);
+    const replaceQueryParameter = useReplaceQueryParameter();
+
+    const onInputChange = ({ target }) => {
+        replaceQueryParameter({
+            key: searchQueryParamName,
+            value: target.value.trim(),
+        });
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        dispatch(fetchSearchSuccess(query));
+    };
 
     return (
-        <SearchWrapper>
+        <SearchWrapper onSubmit={onFormSubmit}>
             <SearchIcon />
             <SearchInput
-                placeholder="Search for movies..."
+                placeholder={
+                    location.pathname.includes("movies")
+                        ? "Search for movies..."
+                        : "Search for people..."
+                }
+                value={query}
+                onChange={onInputChange}
             />
         </SearchWrapper>
     )
