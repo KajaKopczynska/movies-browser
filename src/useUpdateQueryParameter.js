47ab2229ab981bf5861a9b0bab04df9c494aa52b
KajaKopczynska <kaja.a.kopczynska@gmail.com>
2023-06-15 12:11:30 +0200

    created useUpdateQueryParameter

---
+++ src/useUpdateQueryParameter.js
@@ -0,0 +1,18 @@
+import { useLocation, useNavigate } from "react-router-dom"
+import { searchQueryParamName } from "./useQueryParameter";
+
+export const useUpdateQueryParameter = () => {
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    return ({ value }) => {
+        const searchParams = new URLSearchParams();
+
+        if (value === "") {
+            searchParams.delete(searchQueryParamName);
+        } else {
+            searchParams.set(searchQueryParamName, value);
+        }
+        navigate(`${location.pathname}?${searchParams.toString()}`);
+    };
+};
